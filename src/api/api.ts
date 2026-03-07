bbf4c6220da17ede7d0d0b3fa838d8ed37fb5c48
Berke Dönmez <brkdnmz99@gmail.com>
2026-03-07 15:38:07 +0300

    use new page count api endpoint (faster)

--- src/api/api.ts
+++ src/api/api.ts
@@ -22,30 +22,30 @@ export async function getChapterPageCount(
     return FIRST_CHAPTER_PAGES;
   }
 
+  if (lang === MangaLanguage.EN) {
+    return await apiClient
+      .get("/page-count", {
+        // I require the page param also, don't you dare question
+        params: { chapter, page: 1 },
+      })
+      .then((res) => res.data.count)
+      .catch(() => MAX_PAGES_PER_CHAPTER);
+  }
+
+  // lang === TR here
   async function pageExists(page: number): Promise<boolean> {
     const pageUrl = getChapterPageUrl(chapter, page, lang);
 
-    switch (lang) {
-      case MangaLanguage.EN: {
-        const res = await apiClient.head(pageUrl, {
-          validateStatus: () => true,
-        });
-        return res.status !== 404;
-      }
-      case MangaLanguage.TR:
-        return new Promise((resolve) => {
-          const img = new Image();
-          img.onerror = () => resolve(false);
-          img.onload = () => resolve(true);
-          img.src = pageUrl;
-        });
-      default:
-        return false;
-    }
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.onerror = () => resolve(false);
+      img.onload = () => resolve(true);
+      img.src = pageUrl;
+    });
   }
 
   let l = 1;
-  let r = chapter === 1 ? FIRST_CHAPTER_PAGES : MAX_PAGES_PER_CHAPTER;
+  let r = MAX_PAGES_PER_CHAPTER;
 
   while (l < r) {
     const mid = Math.floor((l + r + 1) / 2);
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const ESTIMATED_CHAPTER_COUNT = 1200;
 export const FIRST_CHAPTER_PAGES = 57;
-export const MAX_PAGES_PER_CHAPTER = 30;
+export const MAX_PAGES_PER_CHAPTER = 40; // previously 30, didn't feel enough for some chapters, e.g., chapter 100
 export const AVAILABLE_ZOOM_LEVELS = [1.5, 2] as const;
 export const DOUBLE_TAP_THRESHOLD_MS = 500;
