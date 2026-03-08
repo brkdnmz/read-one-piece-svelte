b948242f42d351cd9467a71ce0bb613086b59122
Berke Dönmez <brkdnmz99@gmail.com>
2026-03-08 09:09:18 +0300

    update mangadenizi url

--- src/api/util.ts
+++ src/api/util.ts
@@ -24,14 +24,14 @@ export function getChapterPageUrl(
     }
 
     case MangaLanguage.TR: {
-      // https://mangadenizi.net/uploads/manga/one-piece-renkli/chapters/0479/03.webp
-      // https://mangadenizi.net/uploads/manga/one-piece-renkli/chapters/667/03.webp
+      // https://mangadenizi.net/storage/manga/one-piece-renkli/chapters/0479/03.webp
+      // https://mangadenizi.net/storage/manga/one-piece-renkli/chapters/667/03.webp
       if (chapter <= 838) {
         const chapterSegment =
           chapter <= 666 ? chapter.toString().padStart(4, "0") : chapter;
 
         const url = new URL(
-          `https://mangadenizi.net/uploads/manga/one-piece-renkli/chapters/${chapterSegment}/${(
+          `https://mangadenizi.net/storage/manga/one-piece-renkli/chapters/${chapterSegment}/${(
             page + 1
           ) // first page is not part of the manga
             .toString()
@@ -42,11 +42,11 @@ export function getChapterPageUrl(
         return url.toString();
       } else {
         // I put else for clarity, it returns in if block anyway
-        // https://mangadenizi.net/uploads/manga/one-piece/chapters/0771/02.webp
+        // https://mangadenizi.net/storage/manga/one-piece/chapters/0771/02.webp
         const chapterSegment = chapter.toString().padStart(4, "0");
 
         const url = new URL(
-          `https://mangadenizi.net/uploads/manga/one-piece/chapters/${chapterSegment}/${(
+          `https://mangadenizi.net/storage/manga/one-piece/chapters/${chapterSegment}/${(
             page + 1
           ) // first page is not part of the manga
             .toString()
