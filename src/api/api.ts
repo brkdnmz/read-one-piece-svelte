b0b48a919b4e0d92e3162e27aeed35fbc30c1cdc
Berke Dönmez <brkdnmz99@gmail.com>
2026-03-09 18:19:39 +0300

    use api for turkish chapter page count also

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,6 @@ import {
 } from "../constants";
 import { MangaLanguage } from "../types";
 import { apiClient } from "./client";
-import { getChapterPageUrl } from "./util";
 
 export async function getChapterCount(): Promise<number> {
   return await apiClient
@@ -22,39 +21,11 @@ export async function getChapterPageCount(
     return FIRST_CHAPTER_PAGES;
   }
 
-  if (lang === MangaLanguage.EN) {
-    return await apiClient
-      .get("/page-count", {
-        // I require the page param also, don't you dare question
-        params: { chapter, page: 1 },
-      })
-      .then((res) => res.data.count)
-      .catch(() => MAX_PAGES_PER_CHAPTER);
-  }
-
-  // lang === TR here
-  async function pageExists(page: number): Promise<boolean> {
-    const pageUrl = getChapterPageUrl(chapter, page, lang);
-
-    return new Promise((resolve) => {
-      const img = new Image();
-      img.onerror = () => resolve(false);
-      img.onload = () => resolve(true);
-      img.src = pageUrl;
-    });
-  }
-
-  let l = 1;
-  let r = chapter === 1 ? FIRST_CHAPTER_PAGES : MAX_PAGES_PER_CHAPTER;
-
-  while (l < r) {
-    const mid = Math.floor((l + r + 1) / 2);
-    if (!(await pageExists(mid))) {
-      r = mid - 1;
-    } else {
-      l = mid;
-    }
-  }
-
-  return l;
+  return await apiClient
+    .get("/page-count", {
+      // I require the page param also, don't you dare question
+      params: { chapter, page: 1, lang },
+    })
+    .then((res) => res.data.count - +(lang === MangaLanguage.TR)) // exclude the first page for TR, mangadenizi's custom cover page
+    .catch(() => MAX_PAGES_PER_CHAPTER);
 }
